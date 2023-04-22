05a7a8fc28252fc781277e52641e60b99b0e4066
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-22 20:08:39 +0000

    Add header file

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * struct fmt_op - function to check for formats
+ * @op: specifier type
+ * @f: The print function to use
+ */
+typedef struct fmt_op
+{
+	char *op;
+	int (*f)();
+} fmt_t;
+
+int _printf(const char *format, ...);
+int listLen(char *str);
+int _putchar(char c);
+int print_ch(va_list list);
+
+#endif
