97f7e6b1e7c4ffdd14b8ad743226c9a03d7882b8
Abdel-AzizMohamed <abdelazizMohamed7530@gmail.com>
2023-04-24 19:27:41 +0000

    Test case 4

--- print_str.c
+++ print_str.c
@@ -8,12 +8,17 @@
 
 int print_str(va_list list)
 {
+	int tmp;
 	int pr_count = 0;
 	char *str = va_arg(list, char*);
 
 	if (str == NULL)
-		str = "(null)";
-
+	{
+		tmp = _printf("(null)");
+		printf("\n%d\n", tmp);
+		return (7);
+	}
+	
 	while (str[pr_count] != '\0')
 		pr_count += _putchar(str[pr_count]);
 
